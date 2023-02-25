b356e0787c54ad0dae4745315c140f1c89f2d913
syahril20 <rodyulo20@gmail.com>
2023-02-25 12:06:33 +0700

    Baruu

--- src/main/java/com/latihan/Controller/HelloController.java
+++ src/main/java/com/latihan/Controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.latihan.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/perpus")
 public class HelloController {
 
     @GetMapping("/")
---
+++ src/main/java/com/latihan/Models/perpus.java
@@ -0,0 +1,46 @@
+package com.latihan.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "perpus")
+public class perpus {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String book_name;
+
+    private String author_name;
+
+    private int harga;
+
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
